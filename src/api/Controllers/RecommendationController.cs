1781ed5aebd5c3e07a19b692cd33c9d2ed43a0fe
Olav Nybo <olavnybo@gmail.com>
2019-01-11 23:28:11 +0100

    Initial controller

---
+++ src/api/Controllers/RecommendationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecommendationController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<string>> Get(string userId)
+        {
+            return Ok("bla");
+        }
+    }
+}
---
+++ src/api/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpPost("{id}/arrived-at-room")]
+        public async Task<ActionResult> ArrivedAtRoom([FromBody] string value)
+        {
+            return Ok(value);
+        }
+    }
+}
